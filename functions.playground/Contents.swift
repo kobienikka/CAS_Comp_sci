bee53722d3ef772886f5b56bcd19ae8c0d6df3b7
Kobie Nikka <kobienikka@gmail.com>
2019-09-11 12:06:54 -0700

    hw

---
+++ functions.playground/Contents.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+//#2
+func square(number: Float) -> Float{
+    let squared = number * number
+    return squared
+}
+
+print(square(number: 5))
+
+//#2
+func five() -> Int {
+    return 5
+}
+
+print(five())
+
+//#3
+func fullName(first: String, last: String) -> String{
+    return first + " " + last
+}
+
+print(fullName(first: "Kobie", last: "Nikka"))
+
+//#4
+func shout(sentence: String) -> String {
+    return sentence + "!"
+}
+
+print(shout(sentence: "Oh hey"))
+
+//#5
+
