3023a808aec643efbcd656f7906fe3d3cf3ab01f
Kobie Nikka <kobienikka@gmail.com>
2019-09-13 08:59:07 -0700

    classify product

--- classifyProduct.playground/Contents.swift
+++ classifyProduct.playground/Contents.swift
@@ -6,5 +6,13 @@ func classifyProduct(num1: Int, num2: Int) -> String{
         return("negative")
     }else if product == 0{
         return("zero")
+    }else if product > 30{
+        return("big")
+    }else if product < 30 && product > 0{
+        return("small")
+    }else{
+        return("IDK what kinda number this")
     }
 }
+
+print(classifyProduct(num1: 3, num2: 5))
