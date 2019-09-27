e7c9e656fab57667ca2cdf9ca960a7453c8d9d89
Kobie Nikka <kobienikka@gmail.com>
2019-09-27 10:02:23 -0700

    chooseYourOwnAdventure

---
+++ noCodeMultipleChoice/noCodeMultipleChoice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  noCodeMultipleChoice
+//
+//  Created by Kobie Nikka on 9/26/19.
+//  Copyright © 2019 Kobie Nikka. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
